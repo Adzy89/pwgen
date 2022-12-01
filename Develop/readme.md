8b9e46c91fed3d5fddd9980d2fa1791bba9f8b00
Adzy89 <adam_pilato@hotmail.com>
2022-12-01 23:19:35 +1100

    added windows prompt box to inform user about password

---
+++ Develop/readme.md
@@ -0,0 +1,23 @@
+# Password Generator
+
+## Description
+Password Generator designed to generate complex passwords, allowing the user to select the length, whether its uppercase or lowercase special characters 
+
+
+## Table of Contents (Optional)
+
+n/a
+
+## Installation
+
+n/a
+
+## Usage
+
+Password generator
+    ```
+
+
+## License
+
+no license
--- Develop/script.js
+++ Develop/script.js
@@ -1,37 +1,40 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];  
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specialCharacters = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
+var lowerCasedCharacters = ['a','b', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w','x', 'y', 'z'];
+var upperCasedCharacters = ['A','B','C', 'D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
-var generatePassword = function () {
-  var generatePassword = window.prompt("Please enter an 8 character Password.");
-  // added windows prompt to alert users to enter a set amount character
-  if (!generatePassword) {
-    return;
-  } 
-  // added windows prompt to alert users to enter a lowercase, uppercase, numeric, and or special characters
-  var index = Math.floor(Math.random() * numericCharacters.length);
 
-  window.prompt("Please enter lowercase, uppercase, numeric, and or special characters");
+function generatePassword(numericCharacters, specialCharacters, lowerCasedCharacters, upperCasedCharacters) {
+  prompt("Please select the password length.");
+
+  if ( numericCharacters += 8 ) {
+  prompt("Please enter Uppercase");
+  }
+
+
   
-
-};
-
+  
+  }
 
 
 // Write password to the #password input
-function writePassword() {
-var password = generatePassword();
+//function writePassword() {
+ 
+
+
+  
+var password = generatePassword();{
+  
+}
 var passwordText = document.querySelector("#password");
  
  passwordText.value = password;
 
-}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
-
-//var specialCharacters = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
-//var lowerCasedCharacters = ['a','b', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w','x', 'y', 'z'];
-//var upperCasedCharacters = ['A','B','C', 'D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];

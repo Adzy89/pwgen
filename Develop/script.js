9fbe9d121f59809a98a51b13ec12a1a90071fb4a
Adzy89 <adam_pilato@hotmail.com>
2022-12-27 20:22:18 +1100

    added lower case statement when false

--- Develop/script.js
+++ Develop/script.js
@@ -13,9 +13,24 @@ function generatePassword(){
   var passwordLength = parseInt(window.prompt("Please select between 8 & 128 Characters"));
 
 //created statement to determine if the pw is less ten 8 char or higher 
-  if (passwordLength < 8 || passwordLength > 128 ) {
-    alert("Please enter a valid Number")
+if (passwordLength < 8 || passwordLength > 128 ) {
+  alert("Please enter a valid Number")
   return;
+}
+
+//created statement for lowercase false value 
+var lowerCase =  confirm (" Would you like to include lowercase letters ?" );
+
+  if ( lowerCase === false ){
+    alert("Please select 'OK' and try again");
+    return;
   }
+
+
+
+
+
+
+
 }
 

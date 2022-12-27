fde15e2c373366cac2d8ad65d1b351ef38c16c76
Adzy89 <adam_pilato@hotmail.com>
2022-12-27 20:16:55 +1100

    added password length statement with window prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,40 +1,21 @@
-// Assignment Code
+
 var generateBtn = document.querySelector("#generate");
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var specialCharacters = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
-var lowerCasedCharacters = ['a','b', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w','x', 'y', 'z'];
-var upperCasedCharacters = ['A','B','C', 'D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var symbols = [ '@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.' ];
+var numbers = ['0123456789'];
+var lowerCharacters = [ 'abcdefghijklmnopqrstuvwxyz' ];
+var upperCharacters = [ 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' ];
 
 
-function generatePassword(numericCharacters, specialCharacters, lowerCasedCharacters, upperCasedCharacters) {
-  prompt("Please select the password length.");
+var characters = []
 
-  if ( numericCharacters += 8 ) {
-  prompt("Please enter Uppercase");
+function generatePassword(){
+  console.log("button clicked");
+  var passwordLength = parseInt(window.prompt("Please select between 8 & 128 Characters"));
+
+//created statement to determine if the pw is less ten 8 char or higher 
+  if (passwordLength < 8 || passwordLength > 128 ) {
+    alert("Please enter a valid Number")
+  return;
   }
-
-
-  
-  
-  }
-
-
-// Write password to the #password input
-//function writePassword() {
- 
-
-
-  
-var password = generatePassword();{
-  
 }
-var passwordText = document.querySelector("#password");
- 
- passwordText.value = password;
-
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
 
